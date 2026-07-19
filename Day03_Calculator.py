5be8ffc065447140c4ffbfa36afea834d872f0ad
Vishwaveer Verma <vishmar1526@gmail.com>
2026-07-19 11:38:14 +0530

    Add files via upload

---
+++ Day03_Calculator.py
@@ -0,0 +1,19 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("The value of", num1, "+", num2, "is:", num1 + num2)
+print("The value of", num1, "-", num2, "is:", num1 - num2)
+print("The value of", num1, "*", num2, "is:", num1 * num2)
+print("The value of", num1, "**", num2, "is:", num1 ** num2)
+print("The value of", num1, "%", num2, "is:", num1 % num2)
+print("The value of", num1, "/", num2, "is:", num1 / num2)
+print("The value of", num1, "//", num2, "is:", num1 // num2)
+# it is the output, if you enter 4 and 2 as the first and second numbers respectively.
+# Enter first number: 4
+# Enter second number: 2
+# The value of 4 + 2 is: 6
+# The value of 4 - 2 is: 2
+# The value of 4 * 2 is: 8
+# The value of 4 ** 2 is: 16
+# The value of 4 % 2 is: 0
+# The value of 4 / 2 is: 2.0
+# The value of 4 // 2 is: 2
