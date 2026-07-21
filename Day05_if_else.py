c9b3b3651906da9c685fc584c18c298fb5d421b2
Vishwaveer Verma <vishmar1526@gmail.com>
2026-07-21 14:24:34 +0530

    Restore age check and voting eligibility logic

--- Day05_if_else.py
+++ Day05_if_else.py
@@ -13,21 +13,3 @@ if age >= 18:
 else:
     print("You are not an adult.")
     print("so you are not eligible to vote.")
-
-# there are elif statements in python which is used to check multiple conditions. The elif statement is short for else if. It allows you to check multiple conditions in a single if-else block. Here's an example:
-
-num = int(input("Enter your marks: "))
-if num >= 50 and num <= 60:
-    print("good")
-elif num >= 60 and num <= 70:
-    print("very good")
-elif num >= 70 and num <= 80:
-    print("excellent")
-elif num >= 80 and num <= 90:
-    print("outstanding")
-elif num >= 90 and num <= 100:
-    print("topper")
-else: 
-    print("failed")  
-print("You are good programmer")
-                    
