5f3db283fab27aba6c07b594534f0e8fa6f84b09
Vishwaveer Verma <vishmar1526@gmail.com>
2026-07-21 14:21:52 +0530

    Add files via upload

---
+++ Day06_elif_uses.py
@@ -0,0 +1,19 @@
+marks = int(input("Enter your marks: "))
+if marks <= 33:
+    print("Status:Failed")
+elif marks >=30 and marks <= 40:
+    print("Grade: F")
+elif marks >= 40 and marks <= 50:
+    print("Grade: E")
+elif marks >= 50 and marks <=60:
+    print("Grade: D")
+elif marks >= 60 and marks <=70:
+    print("Grade: C")
+elif marks >= 70 and marks <=80:
+    print("Grade: B")
+elif marks >= 80 and marks <=90:
+    print("Grade: A")
+elif marks >= 90 and marks <=100:
+    print("Grade: A++")
+else:
+    print("invalid")
