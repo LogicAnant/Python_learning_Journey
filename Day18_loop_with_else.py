9591ff0e8bcf7ca72bf895f3a332dc1541f8dae6
Vishwaveer Verma <vishmar1526@gmail.com>
2026-08-03 14:23:00 +0530

    Add files via upload

---
+++ Day18_loop_with_else.py
@@ -0,0 +1,13 @@
+# For loop with else
+for i in range(1, 6):
+    print("For loop value:", i)   # Print each value
+else:
+    print("For loop finished successfully")   # Runs when loop ends normally
+
+# While loop with else
+count = 1
+while count <= 5:
+    print("While loop value:", count)   # Print each value
+    count += 1
+else:
+    print("While loop finished successfully")   # Runs when loop ends normally
